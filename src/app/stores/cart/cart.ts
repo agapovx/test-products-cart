8ba706068c4ec128f1c4c58994e5bf40f8fc6a3a
Ilya Agapov <ilyahljm@gmail.com>
2020-02-23 18:47:27 +0300

    Correct types

--- src/app/stores/cart/cart.ts
+++ src/app/stores/cart/cart.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from 'react';
 import { makeStateUndoable, UndoableState } from 'app/core/undo';
+import { Coupon } from 'app/stores/coupons/coupons';
 
 import { CartStateActions } from './types';
 
@@ -8,17 +9,23 @@ export type CartItem = {
   count: number;
 }
 
-const cart = (state: CartItem[] = [], action: CartStateActions) => {
+export type CartState = {
+  items: CartItem[];
+  coupon?: Coupon['id'];
+}
+
+const cart = (state: CartState, action: CartStateActions) => {
   switch (action.type) {
     default:
       return state;
   }
 }
 
-export const undoableCart = makeStateUndoable<CartItem[]>(cart, []);
+export const undoableCart = makeStateUndoable<CartState, CartStateActions>(cart, { items: [] });
 
 export const CartActionsContex = createContext({});
 export const useCartActionsContext = () => useContext(CartActionsContex);
 
-export const CartStateContext = createContext<UndoableState<CartItem[]>>({ past: [], present: [], future: [] });
+export const CartStateContext = createContext
+  <UndoableState<CartItem[]>>({ past: [], present: [], future: [] });
 export const useCartStateContext = () => useContext(CartStateContext);
